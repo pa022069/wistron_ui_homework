5a980d8ecce02a3b6cc04e97697cff62d00a274c
Wistronits <pa022069@gmail.com>
2023-02-09 10:37:16 +0800

    add docker build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    svgr(),
-    react(),
-    viteTsconfigPaths(),
-  ],
+  plugins: [svgr(), react(), viteTsconfigPaths()],
   base: "./",
-})
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
+  },
+});
