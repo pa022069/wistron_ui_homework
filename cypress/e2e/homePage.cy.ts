07dcff1c036c98a6e69dede075c1b87cd68ac6e4
Wistronits <pa022069@gmail.com>
2023-02-21 15:05:06 +0800

    modify cypress

---
+++ cypress/e2e/homePage.cy.ts
@@ -0,0 +1,14 @@
+describe("template spec", () => {
+  it("passes", () => {
+    cy.viewport(1366, 768);
+    cy.visit("http://localhost:5173");
+    cy.get("#firstName").type("firstName").should("have.value", "firstName");
+    cy.get("#lastName").type("lastName").should("have.value", "lastName");
+    cy.get("#email")
+      .type("test@test.com")
+      .should("have.value", "test@test.com");
+    cy.get("#password").type("password@22").should("have.value", "password@22");
+    cy.get("#checkbox").check().should("be.checked", true);
+    cy.contains("Create an Free Account!").click();
+  });
+});
