cefd7f6deffdbe24a2415589330dc0229accea3b
Wistronits <pa022069@gmail.com>
2023-02-22 11:48:56 +0800

    fix

--- cypress/e2e/homePage.cy.ts
+++ cypress/e2e/homePage.cy.ts
@@ -1,7 +1,7 @@
 describe("template spec", () => {
   it("passes", () => {
     cy.viewport(1366, 768);
-    cy.visit("http://localhost:5173");
+    cy.visit("http://localhost:3000");
     cy.get("#firstName").type("firstName").should("have.value", "firstName");
     cy.get("#lastName").type("lastName").should("have.value", "lastName");
     cy.get("#email")
--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,6 @@ export default defineConfig({
     },
     host: true,
     strictPort: true,
-    port: 5173,
+    port: 3000,
   },
 });
